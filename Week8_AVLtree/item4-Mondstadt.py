fde8dadf58b754293c9b5339491e1b4a0f4f1426
Fair <pattaradanai4290@gmail.com>
2022-10-30 19:13:48 +0700

    ...

--- Week8_AVLtree/item4-Mondstadt.py
+++ Week8_AVLtree/item4-Mondstadt.py
@@ -9,17 +9,22 @@ print(sum(lst))
 for q in inp_[1].split(','):
     x,y = list(map(int, q.split()))
     #print(2*x+1, 2*x+2)
-    temp1 = lst[x] 
-    if 2*x+1 < len(lst):    
-        temp1 += lst[2*x+1]
-    if 2*x+2 < len(lst):
-        temp1 += lst[2*x+2]
-    temp2 = lst[y]
-    if 2*y+1 < len(lst):
-        temp2 += lst[2*y+1]
-    if 2*y+2 < len(lst):
-        temp2 += lst[2*y+2]
+    temp1 = lst[x]
     print(x, end='')
+    i = x
+    while 2*i+1 < len(lst):
+        if 2*x+1 < len(lst):    
+            temp1 += lst[2*x+1]
+        if 2*x+2 < len(lst):
+            temp1 += lst[2*x+2]
+        
+        temp2 = lst[y]
+        if 2*y+1 < len(lst):
+            temp2 += lst[2*y+1]
+        if 2*y+2 < len(lst):
+            temp2 += lst[2*y+2]
+        
+    print(temp1 , temp2)
     if temp1 == temp2:
         print("=", end='')
     elif temp1<temp2:
